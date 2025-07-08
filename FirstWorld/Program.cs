42ea0a500ef7c3034880fda18f28fe5fed8663bf
Riazbs23 <riazsorkar709@gmail.com>
2025-07-08 19:13:55 +0600

    Changes

--- FirstWorld/Program.cs
+++ FirstWorld/Program.cs
@@ -7,14 +7,27 @@
             //Console.WriteLine("Hello, World!");
 
             //C# Variables
-            char word = 'Z';
-            string name = "Zaman";
-            double cgpa = 3.10;
-            int a = 10;
-            int b = 20;
-            int result = a + b;
-            Console.Write("My Name is " + name + " My Fav word is " + word + " My CGPA is " + cgpa + " I have only " + result + " taka only");
+            //char word = 'Z';
+            //string name = "Zaman";
+            //double cgpa = 3.10;
+            //int a = 10;
+            //int b = 20;
+            //int result = a + b;
+            //Console.Write("My Name is " + name + " My Fav word is " + word + " My CGPA is " + cgpa + " I have only " + result + " taka only");
 
+
+            ////C# User Input
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Thanks"+name);
+
+
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            
         }
     }
 }
