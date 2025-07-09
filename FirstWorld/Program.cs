2f70ee3570380a54956f35144e3997f78cf57461
Riazbs23 <riazsorkar709@gmail.com>
2025-07-09 17:47:33 +0600

    Update

--- FirstWorld/Program.cs
+++ FirstWorld/Program.cs
@@ -2,11 +2,31 @@
 {
     internal class Program
     {
+
+        //constructor
+         public string myName;
+
+            public Program()
+            {
+                myName = "This is Riazzzz";
+            }
+    
+
+
+
+        //FUNCTION
+        //static void wellcomeMassage()
+        //{
+        //    Console.WriteLine("Wellcome To Bangladesh");
+        //}
         static void Main(string[] args)
         {
+            Program prog1 = new Program(); 
+            Console.WriteLine(prog1.myName);
+            //wellcomeMassage();
             //Console.WriteLine("Hello, World!");
 
-            //C# Variables
+            //C# Variables--------------------
             //char word = 'Z';
             //string name = "Zaman";
             //double cgpa = 3.10;
@@ -16,18 +36,50 @@
             //Console.Write("My Name is " + name + " My Fav word is " + word + " My CGPA is " + cgpa + " I have only " + result + " taka only");
 
 
-            ////C# User Input
+            ////C# User Input------------
             //string name = Console.ReadLine();
             //Console.WriteLine("Thanks"+name);
 
 
-            int i = 0;
-            while (i < 5)
-            {
-                Console.WriteLine(i);
-                i++;
-            }
-            
+            //LOOP----------------
+            //int i;
+            //while (i < 5)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+
+            //for (int i = 0; i <= 5; i++) {
+            //    Console.WriteLine(i);
+            //}
+
+            //condition----------------
+            //bool a = true;
+            //if (a == true)
+            //{
+            //    Console.WriteLine("Sotti");
+            //}
+
+
+            //Array 
+
+            //int[] num = new int [5];
+
+            //Console.Write("Enter Number ");
+            //num[0] = Convert.ToInt32 (Console.ReadLine());
+
+            //Console.Write("Enter Number ");
+            //num[1] = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter Number ");
+            //num[2] = Convert.ToInt32(Console.ReadLine());
+
+
+            //for (int i = 0; i < num.Length; i++) { 
+            //    Console.Write(num[i]);
+            //}
+
         }
     }
 }
